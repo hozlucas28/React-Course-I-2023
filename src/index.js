2cfba922e49946e2cc93e3782cb426db3970ade8
hozlucas28 <hozlucas28@gmail.com>
2023-01-08 18:41:56 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const Li = ({ children, state, house, age }) => {
+	console.log(state, house, age);
+	return (
+		<li>
+			{children} {state}
+		</li>
+	);
+};
+
+const X = () => (
+	<ul>
+		<Li state={'feliz'} house={false} age={21}>
+			Cerdo
+		</Li>
+		<Li state={'triste'} house={true} age={32}>
+			Cerdo
+		</Li>
+		<Li state={'amargo'} house={false} age={18}>
+			Cerdo
+		</Li>
+	</ul>
 );
+
+root.render(<X />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
