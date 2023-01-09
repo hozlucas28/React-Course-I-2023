b83493e6ba023a1759db2e9e0bf8c702b8a6d408
hozlucas28 <hozlucas28@gmail.com>
2023-01-09 19:47:11 -0300

    Upload lesson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import Button from './Button';
+import { Component } from 'react';
 
-const array = ['cerdo feliz', 'cerdo triste', 'cerdo emocionado'];
+// Componente
+class Button extends Component {
+	state = {};
 
-const App = () => {
-	const miVar = false;
-
-	if (miVar) {
-		return <p>Mi variable es True.</p>;
+	constructor(props) {
+		super(props);
+		console.log('Constructor ', props);
 	}
 
-	return (
-		<div>
-			<h1 onClick={(element) => console.log('Click', element)}>¡Hola Mundo!</h1>
-			{array.map((element) => (
-				<p key={element}>{element}</p>
-			))}
-			<Button onClick={() => console.log('Clickeado')}>Enviar</Button>
-		</div>
-	);
-};
+	componentDidMount() {
+		console.log('componentDidMount');
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		console.log('componentDidUpdate ', prevProps, ' ', prevState);
+	}
+
+	componentWillUnmount() {
+		console.log('componentWillUnmount ', this.props, ' ', this.state);
+	}
+
+	render() {
+		console.log('Ejecutando método de render de Button');
+		return <button onClick={() => this.setState({ prop: 1 })}>Enviar en Button</button>;
+	}
+}
+
+// Aplicación
+class App extends Component {
+	state = {
+		value: 3
+	};
+
+	render() {
+		console.log(this.state);
+		return (
+			<div>
+				<p>¡Hola Mundo!</p>
+				{this.state.value === 3 ? <Button pig="Happy" /> : null}
+				<button className={this.state.value} onClick={() => this.setState({ value: 2 })}>
+					Enviar en App
+				</button>
+			</div>
+		);
+	}
+}
 
 export default App;
